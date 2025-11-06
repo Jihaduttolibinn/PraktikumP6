7ce303d7004fb1227e982ea5ae7ee2cfd6f6f7f7
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-06 15:34:20 +0700

    Menambahkan fun FormIsian

---
+++ app/src/main/java/com/example/praktikump6/view/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.praktikump6.view
+
+import android.R
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
+){
+
+}
