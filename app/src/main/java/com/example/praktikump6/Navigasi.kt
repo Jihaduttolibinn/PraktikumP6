d12be50ff48deb016d6b31cfe44e46cd5183733f
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-06 17:02:11 +0700

    Menambahkan private fun

--- app/src/main/java/com/example/praktikump6/Navigasi.kt
+++ app/src/main/java/com/example/praktikump6/Navigasi.kt
@@ -44,4 +44,10 @@ fun DataApp(
         }
     }
 }
+private fun cancelAndBackToFormulirKu(
+    navController: NavHostController
+){
+    navController.popBackStack(route = Navigasi.Formulirku.name,
+        inclusive = false)
+}
 
